7788b7efb1f3089f95c0e6d4089d8259b65b381d
Oladoyeen <oladoyintemitope@gmail.com>
2023-02-28 22:43:31 +0100

    more JS

--- main.js
+++ main.js
@@ -20,9 +20,24 @@ for (let key of keys) {
 
             display_output.innerHTML = result;
         } else if (value == "brackets") {
-            if (input.indexOf("(")) == -1 || input.indexOf("(") != -1 && input.indexOf(")") != -1 && input.lastIndexOf("(") < input.lastIndexOf(")") {
+            if (
+            input.indexOf("(") == -1 || 
+            input.indexOf("(") != -1 && 
+            input.indexOf(")") != -1 && 
+            input.lastIndexOf("(") < input.lastIndexOf(")")) {
                 input += "(";
-            } else if (input.indexOf("(") )  
+            } else if (
+            input.indexOf("(") != -1 && 
+            input.indexOf(")") == -1 ||
+            input.indexOf("(") != -1 &&
+            input.indexOf(")") != -1 &&
+            input.lastIndexOf("(") > input.lastIndexOf(")")
+            ) {
+                input -= ")";
+            } 
+            display_input.innerHTML =input;
+        } else {
+            input += value;
         }
     })
 }
