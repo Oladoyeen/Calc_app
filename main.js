5d8508d934ededbeec280e01f55b606e543f875e
Oladoyeen <oladoyintemitope@gmail.com>
2023-03-02 22:54:10 +0100

    hard JS

--- main.js
+++ main.js
@@ -14,11 +14,11 @@ for (let key of keys) {
             display_output.innerHTML = "";
         } else if (value == "backspace") {
             input = input.slice(0, -1);
-            display_input.innerHTML = input;
+            display_input.innerHTML = cleanInput(input);
         } else if (value == "=") {
             let result = eval(input);
 
-            display_output.innerHTML = result;
+            display_output.innerHTML = CleanOutput(result);
         } else if (value == "brackets") {
             if (
             input.indexOf("(") == -1 || 
@@ -35,10 +35,10 @@ for (let key of keys) {
             ) {
                 input += ")";
             } 
-            display_input.innerHTML =input;
+            display_input.innerHTML = cleanInput(input);
         } else {
             input += value;
-            display_input.innerHTML = input;
+            display_input.innerHTML = cleanInput(input);
         }
     })
 }
@@ -49,8 +49,42 @@ function cleanInput(input) {
     for (let i = 0; i < input_array_length; i++) {
         if (input_array[i] == "*") {
             input_array[i] = ' <span class="operator">x</span> ';
-        } else if (inpt_array[i] = "*") {
-            input_array[i] = ' <span class="operator">x</span> ';
+        } else if (input_array[i] = "/") {
+            input_array[i] = ' <span class="operator">÷</span> ';
+        } else if (input_array[i] = "+") {
+            input_array[i] = ' <span class="operator">+</span> ';
+        } else if (input_array[i] = "-") {
+            input_array[i] = ' <span class="operator">-</span> ';
+        } else if (input_array[i] = "(") {
+            input_array[i] = '<span class="brackets">(</span>';
+        } else if (input_array[i] = ")") {
+            input_array[i] = '<span class="brackets">)</span>';
+        } else if (input_array[i] = "%") {
+            input_array[i] = '<span class="percent">%</span>';
+        } 
+    }
+
+    return input_array.join("");
+}
+
+function CleanOutput (output); {
+    let output_string = output.toString();
+    let decimal = output_string.split(".")[1];
+    output_string = output_string.split(".")[0];
+
+    let output_array = output_string.split("");
+
+    if (output_array.length > 3) {
+        for (let i = output_array.length - 3; i > 0; i -= 3) {
+            output_array.splice(i, 0, ",");
         }
     }
+
+    if (decimal) {
+        output_array.push(".");
+        output_array.push(decimal);
+    }
+
+    return output_array.join("");
+
 }
